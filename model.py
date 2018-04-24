72eba5bbadb9d78cd0191e503db1eeb4809d5151
bhumphrey0x20 <38211805+bhumphrey0x20@users.noreply.github.com>
2018-04-23 21:23:17 -0500

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,11 @@
+"""
+Udacity Self-Driving Car Nanodegree, Term 1, Project 3: Behavioral Cloning
+
+Uses a Convolutional Neural Network to mimic driving habits learned from test data to drive a car simulator 
+automonously around a track.
+BH 4/23/2018
+"""
+
 import numpy as np
 import csv
 import cv2
@@ -11,15 +19,16 @@ from keras import backend as k
 EPOCHS = 4
 
 '''
-	read csv file, load data into array. 
-	This code taken from lecture
+read csv file, load data into array. 
+Most of this code was taken from lecture
 '''
-
+#Various data paths
 data_path = './data'
 data_path_1 = './data/jerky'
 data_path_2 = './data/bridge'
 data_path_3 = './data/jungle'
 
+# Create a list of data paths
 data= [data_path, data_path_1, data_path_2, data_path_3]
 
 left_cam_Flag = 0
@@ -60,12 +69,19 @@ for dat in data:
 			images.append(image)
 			measurements.append(measurement)
 
-X_train = np.array(images) #.reshape(-1,160,320,1)
+X_train = np.array(images) 
 y_train = np.array(measurements)
 
 print('Done')	
 
+"""
+Architecture used was adapted into Keras from: 
+https://github.com/jeremy-shannon/CarND-Traffic-Sign-Classifier-Project/blob/master/Traffic_Sign_Classifier.ipynb
+
+"""
+
 Input = Input(shape=(160,320,3))
+# Normalize (-0.5 to 0.5) and Crop images
 x = Lambda(lambda x: (x/255.) - 0.5)(Input)
 x= Cropping2D(cropping= ((50,20), (0,0)), input_shape=(3,160,320))(x)
 
@@ -85,14 +101,17 @@ layer3 = merge([flat_a, flat_b], mode='concat', concat_axis=1)
 logits = Dense(1, name='d4')(layer3)
 print(logits.get_shape() )
 
-
-
 # compile model and get loss function
 model = Model(input=Input, output=logits)
 model.compile(loss='mse', optimizer='adam')
 history_loss = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch= EPOCHS)
 
 model.save('model.h5')
+
+""" 
+Prints training loss and validation loss
+This code was also taken from lecture
+"""
 
 print(history_loss.history.keys())
 plt.plot(history_loss.history['loss'])
